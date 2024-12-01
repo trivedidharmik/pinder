48f6082690fa4fe2e71d51fb78609263dd22d2bf
trivedidharmik <dmtrivedi2@gmail.com>
2024-12-01 16:24:52 -0400

    Settings page Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "ca.unb.mobiledev.pinder"
-        minSdk = 27
+        minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
