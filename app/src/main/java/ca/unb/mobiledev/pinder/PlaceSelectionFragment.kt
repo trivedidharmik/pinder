f32cba612dc212f01837a21f9bd899bc3df57036
trivedidharmik <dmtrivedi2@gmail.com>
2024-12-02 21:39:48 -0400

    Clean up

--- app/src/main/java/ca/unb/mobiledev/pinder/PlaceSelectionFragment.kt
+++ app/src/main/java/ca/unb/mobiledev/pinder/PlaceSelectionFragment.kt
@@ -10,7 +10,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ca.unb.mobiledev.pinder.databinding.FragmentPlaceSelectionBinding
